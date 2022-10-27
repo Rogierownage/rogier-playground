ddadcae12fccc01a2d1e1e6953bbc25b7a32fe93
Rogier van den Berg <rogier.vandenberg@way2web.nl>
2022-10-27 13:21:41 +0200

    update export translations script

--- php-scripts/export-translations.php
+++ php-scripts/export-translations.php
@@ -16,18 +16,15 @@ foreach ($allDutch as $dutch) {
     $result[] = [$dutch->namespace, $dutch->group, $dutch->name, $dutch->value, $english->value];
 }
 
-//
-
 $header = ['Namespace','Groep','Naam','Nederlands','Engels'];
 $records = $result;
 
-//load the CSV document from a string
 $csv = Writer::createFromString();
 
-//insert the header
+$csv->setDelimiter(';');
+
 $csv->insertOne($header);
 
-//insert all the records
 $csv->insertAll($records);
 
 file_put_contents(storage_path('app/export.csv'), $csv->toString());
