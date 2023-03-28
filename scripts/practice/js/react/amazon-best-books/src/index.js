61ba41791251f00f2925a757006807c989eab21f
Rogier van den Berg <rogier.vandenberg@way2web.nl>
2023-03-28 14:57:41 +0200

    implement getBook function

--- scripts/practice/js/react/amazon-best-books/src/index.js
+++ scripts/practice/js/react/amazon-best-books/src/index.js
@@ -11,23 +11,42 @@ function App() {
 }
 
 const Button = ({text, onClick}) => (
-    <button onClick={onClick}>{text}</button>
+    <button type="button" onClick={onClick}>{text}</button>
 );
 
-const BookList = () => (
-    <section className="booklist">
-        {books.map((book) => (
-            <Book
-                {...book}
-                key={book.title}
-            >
-                {book.hasButton ? <Button text="Add to cart" onClick={() => alert('Added: ' + book.title)} /> : null}
-            </Book>
-        ))}
-    </section>
-);
+const BookList = () => {
+    const getBook = (id) => {
+        return books.find((book) => book.id === id);
+    };
 
-const Book = ({ style = { color: '#a9a9a9' }, imageUrl, title, author, children}) => {
+    return (
+        <section className="booklist">
+            {books.map((book) => (
+                <Book
+                    {...book}
+                    key={book.title}
+                    getBook={getBook}
+                >
+                    {book.hasButton ? (
+                        <div>
+                            <Button text="Add to cart" onClick={() => alert('Added: ' + book.title)} />
+                            <form onSubmit={(e) => {
+                                e.preventDefault();
+
+                                alert('submit');
+                            }}>
+                                <input onChange={(e) => console.log([e.target.name, e.target.value])} type="text" name="title"></input>
+                                <button type="submit">Submit</button>
+                            </form>
+                        </div>
+                    ) : null}
+                </Book>
+            ))}
+        </section>
+    )
+};
+
+const Book = ({ style = { color: '#a9a9a9' }, imageUrl, title, author, id, getBook, children}) => {
     const titlePrefix = 'Title:';
 
     return (
@@ -36,23 +55,27 @@ const Book = ({ style = { color: '#a9a9a9' }, imageUrl, title, author, children}
             <h2>{titlePrefix} {title}</h2>
             {children}  
             <p style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem' }}>By {author}</p>
+            <button onClick={() => console.log(getBook(id).title)}>Find book</button>
         </article>
     );
 };
 
 const books = [
     {
+        id: 1,
         title: 'Young Forever: The Secrets to Living Your Longest, Healthiest Life',
         author: 'Dr. Mark Hyman MD',
         imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/81dL13NFc3L._AC_UL600_SR600,400_.jpg',
         hasButton: true
     },
     {
+        id: 2,
         title: 'Things We Hide from the Light(Knockemout Series, 2)',
         author: 'Lucy Score',
         imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/71h9CawUxpL._AC_UL600_SR600,400_.jpg',
     },
     {
+        id: 3,
         title: 'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
         author: 'James Clear',
         imageUrl: './images/atomic-habits.jpg',
