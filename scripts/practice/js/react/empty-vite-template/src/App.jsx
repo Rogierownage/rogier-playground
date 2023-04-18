9989d36b9af251cb04b6478d873457cf7074dfe4
Rogier van den Berg <rogier.vandenberg@way2web.nl>
2023-04-18 17:34:06 +0200

    setup vite project

---
+++ scripts/practice/js/react/empty-vite-template/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Paragraph from './components/Paragraph'
+
+function App() {
+  return (
+    <div className="App">
+        <Paragraph content="Hello, world!" />
+    </div>
+  )
+}
+
+export default App
---
+++ scripts/practice/js/react/empty-vite-template/src/components/Paragraph.jsx
@@ -0,0 +1,5 @@
+const Paragraph = ({content}) => (
+    <p>{content}</p>
+);
+
+export default Paragraph;
