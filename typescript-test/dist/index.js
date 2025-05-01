5738feb434089c50055178fd21984b58ad0c8e16
Rogier van den Berg <rogier.vandenberg@paqt.com>
2025-05-01 14:38:55 +0200

    remove dist files from version control

--- typescript-test/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let information = { hasPets: true };
-console.log(information);
-// You can actually reassign the entire object, but it will keep its type definition. The following is valid.
-information = { hasPets: false };
-console.log(information);
-//# sourceMappingURL=index.js.map
