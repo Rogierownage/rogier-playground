59193d5c6604c7ce3236588faaf9d90a8e91184d
Rogier van den Berg <rogier.vandenberg@paqt.com>
2025-12-01 16:43:12 +0100

    make it easier to setup and test that dockerhero works

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use Illuminate\View\View;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
     public function index(): View
     {
+        dump(User::firstOrFail()->only(['id', 'name', 'email']));
+
         return view('dashboard.index');
     }
 }
